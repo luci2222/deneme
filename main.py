bac63fcd8d47e77906167e74d581f7d8eb2d6b0f
ensarerturkDeepcase <ensarerturkresmi@gmail.com>
2021-07-27 11:48:53 +0300

    carpma eklendi

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ def topla(x,y):
 def hello():
     print("Merhaba Dünya")
     
+def carpma(x,y):
+    return x*y
+    
 a = int(input("Lütfen birinci sayıyı girin :"))
 b = int(input("Lütfen ikinci sayıyı girin :"))
 
 print(topla(a,b))
 hello()
+carpma(a,b)
