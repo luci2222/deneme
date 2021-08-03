e8a7b3d7e7b3eeeac2bceed9f54992441c41bd5e
deneme <deneme@gmail.com>
2021-08-03 18:02:18 +0300

    cherry-pick

---
+++ main.py
@@ -0,0 +1,17 @@
+def topla(x,y):
+    return x + y
+
+def hello():
+    print("Merhaba Dünya")
+    
+def carpma(x,y):
+    return x*y
+    
+a = int(input("Lütfen birinci sayıyı girin :"))
+b = int(input("Lütfen ikinci sayıyı girin :"))
+
+print(topla(a,b))
+hello()
+carpma(a,b)
+
+## deneme123 ##
